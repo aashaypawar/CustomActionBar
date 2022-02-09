5ac406d30ce26ed56afdedaa8b45d60ab3af5117
Aashay Pawar <41710824+aashaypawar@users.noreply.github.com>
2022-02-09 16:48:46 +0530

    Initial commit

---
+++ app/src/main/java/org/geeksforgeeks/customactionbar/MainActivity.kt
@@ -0,0 +1,15 @@
+package org.geeksforgeeks.customactionbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        this.supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar!!.setDisplayShowCustomEnabled(true)
+        supportActionBar!!.setCustomView(R.layout.my_layout)
+    }
+}
